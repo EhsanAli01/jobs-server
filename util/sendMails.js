063f9680285e07632e603597247a406f0b947fc7
Ehsan Ali <148131941+EhsanAli01@users.noreply.github.com>
2024-10-16 13:51:56 +0500

    updated

--- util/sendMails.js
+++ util/sendMails.js
@@ -17,7 +17,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
+}); 
 
 transporter.verify((error, success) => {
   if (error) {
